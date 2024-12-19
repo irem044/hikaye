38753f39800a55c30662300dc553683c0a7cd75c
burkira <147352689+burakgokmenn@users.noreply.github.com>
2024-12-19 16:56:53 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -4,5 +4,27 @@
 int main() {
 
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
+  printf("Bu bir örnektir uygulamayızı kendi satırınıza alt satıra inmeden bu şekil yazın ");          //ÖRNEK (adın soyadın işte :d ); SOL TARAFINA YAZACAKSINIZ
+          //iREM TOKTAŞ
+          // EMSAL İREM DEMİREL
+          //İREM TOPSAKAL
+          //Mustafa Saput
+          //Yasin ERGÜT
+          //melike yilmaz
+          //Ayşe Nur Karakoyunlu
+          //elif zeren boyraz
+          //İbrahim TEK
+          //Sıla Kuzan
+          //Emine Zehra Köseoğlu
+          //Muhammed eren zengel
+          //Ömer Faruk korkunç
+          //Aminenur CAN
+          //BERRA ADISLI
+          //Eftal taha karataş
+          //Oğuzhan koylan
+          //Hasret arslan
+          //Emin Akpolad
+          //Duygu Yüksel
 
+  
 }
