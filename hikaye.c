f8cebf6ac57eea92c905c0cb2d6b6818c2272ccf
burkira <burkira58t@gmail.com>
2024-12-19 16:41:34 +0300

    Initial commit

---
+++ hikaye.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+
+
+int main() {
+
+printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
+
+}
